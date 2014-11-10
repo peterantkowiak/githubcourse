3e5245d707686ead2d71fcdb48d507712129bb3e
Ahmed <elgabbas@outlook.com>
2014-11-10 11:50:59 +0100

    ADDING SOME CODE

---
+++ Ahmed - New R Code.R
@@ -0,0 +1,17 @@
+# Code from:
+# https://github.com/Pakillo/R-GIS-tutorial/blob/master/R-GIS_tutorial.md
+library(sp)  # classes for spatial data
+library(raster)  # grids, rasters
+library(rasterVis)  # raster visualisation
+library(maptools)
+library(rgeos)
+# and their dependencies
+
+library(dismo)
+
+mymap <- gmap("France")  # choose whatever country
+plot(mymap)
+
+mymap <- gmap("France", type = "satellite")
+plot(mymap)
+
